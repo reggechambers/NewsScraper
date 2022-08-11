33704095cb94230e26767ae239772c7245596115
reggechambers <78483402+reggechambers@users.noreply.github.com>
2022-08-11 07:58:14 -0400

    Main

---
+++ main.py
@@ -0,0 +1,15 @@
+#dependencies
+import requests
+from bs4 import BeautifulSoup
+
+#url to scrape from
+url = "https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight"
+
+#URL request and parser
+r = requests.get(url)
+
+#Selecting and formatting data 
+#Print results
+
+
+
